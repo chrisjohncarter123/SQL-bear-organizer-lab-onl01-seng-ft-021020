112728d0ba72d6176d7deab3895d24b056b3667b
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-11 03:36:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,15 @@
-INSERT INTO bears VALUES ("Mr. Chocolate", 1,, "male", "red", "happy", true)
+INSERT INTO bears VALUES ("Mr. Chocolate", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES ("Rowdy", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES ("Tabitha", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES ("Sergeant Brown", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES ("Melissa", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES ("Grinch", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES ("Wendy", 1, "m", "red", "happy", 1)
+
+INSERT INTO bears VALUES (NULL, 1, "m", "red", "happy", 1)
